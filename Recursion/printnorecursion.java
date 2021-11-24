61134da7db3781bdca633c4826741e9abb137aba
Shivam Jaiswal <jaiswalshivam373@gmail.com>
2021-11-24 22:38:53 +0530

    printno

---
+++ Recursion/printnorecursion.java
@@ -0,0 +1,16 @@
+package com.company.printno;
+
+public class recursion {
+    public static void main(String[] args) {
+
+        print(1);
+    }
+    static void print(int n){
+        if(n==5){
+            System.out.println(5);
+            return;
+        }
+        System.out.println(n);
+        print(n+1);
+    }
+}
