f3ecfa53c90b069f825255952ea3b112d34cc2fb
Shivam Jaiswal <jaiswalshivam373@gmail.com>
2021-12-21 22:18:38 +0530

    Problem 2011

---
+++ Array/2011_final_value_after_performing_operations.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int finalValueAfterOperations(String[] operations) {
+        int x = 0;
+        for(String op:operations){
+            if(op.contains("++")){
+                x++;
+            } else if(op.contains("--")){
+                x--;
+            } 
+        }
+        return x;
+    }     
+}
