de4e3ff30214548bedffb92866a8a636113d10b1
Shivam Jaiswal <jaiswalshivam373@gmail.com>
2021-12-25 22:50:19 +0530

    168

---
+++ Array/excelforcolumetitle.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String convertToTitle(int columnNumber) {
+        int base = 'Z'-'A'+1;
+    StringBuilder sb = new StringBuilder();
+    while(columnNumber!=0){
+        int rem = columnNumber%base;
+        if(rem!=0)
+            sb.append((char)('A'+ rem -1));
+        else {
+            sb.append('Z');
+            columnNumber--;
+        }
+
+        columnNumber /= base;
+    }
+    return sb.reverse().toString(); 
+    }
+}
