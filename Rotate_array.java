0da5b39591fd54837f15828566c643dd27ac3bcf
Shivam Jaiswal <jaiswalshivam373@gmail.com>
2022-01-19 20:29:07 +0530

    189

---
+++ Rotate_array.java
@@ -0,0 +1,20 @@
+class Solution {
+    public void rotate(int[] nums, int k) 
+    {
+        k=k%nums.length;
+        reverse(0,nums.length-1,nums);
+        reverse(0,k-1,nums);
+        reverse(k,nums.length-1,nums);
+    }
+    private void reverse(int start,int end,int nums[])
+    {
+        while(start<end)
+          {
+            int temp=nums[start];
+            nums[start]=nums[end];
+            nums[end]=temp;
+            start++;
+            end--;
+          }
+    }
+} 
