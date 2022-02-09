b8242b3a9843a19568e77042a7a67cf215cf5369
Shivam Jaiswal <jaiswalshivam373@gmail.com>
2022-02-09 22:50:51 +0530

    1323

---
+++ Array/maximum_of_69_no.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maximum69Number (int num) {
+      String s=Integer.toString(num);
+      char ch[]=s.toCharArray();
+      for(int i=0;i<ch.length;i++)
+      {
+          if(ch[i]=='6')
+          {
+              ch[i]='9';
+              return Integer.parseInt(String.valueOf(ch));
+          }
+      }
+        return num;
+      
+    }
+        
+      }   
+        
