c0521f6280811e1ef608e9bc66d13009b312795b
Shivam Jaiswal <jaiswalshivam373@gmail.com>
2022-04-15 22:09:33 +0530

    744

---
+++ Array/744 _Find _Smallest_ Letter _Greater _Than _Target.java
@@ -0,0 +1,19 @@
+class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+         int start = 0;
+        int end = letters.length-1;
+        if(target >= letters[letters.length-1]){
+            return letters[0];
+        }
+        while(start <= end){
+            int mid = start + (end-start)/2;
+            if(target >= letters[mid]){
+                start = mid+1;
+            }
+            else if(target < letters[mid]){
+                end = mid-1;
+            }
+        }
+        return letters[start];
+    }
+}
