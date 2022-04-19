ea8d13f6cd84b93e32eeb6ce96f8673b722c68d2
Shivam Jaiswal <jaiswalshivam373@gmail.com>
2022-04-19 23:21:40 +0530

    1337

---
+++ Array/1337_ The _K _Weakest _Rows _in a _Matrix.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[] kWeakestRows(int[][] mat, int k) {
+        int count[]=new int[mat.length];
+        int ans[]=new int[k];
+        for(int i=0;i<mat.length;i++)
+        {
+            int soldier=0;
+            for(int j=0;j<mat[0].length;j++)
+            {
+                if(mat[i][j]==1)
+                {
+                    soldier++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            count[i]=soldier*1000 + i;
+        }
+        Arrays.sort(count);
+        for(int i=0;i<k;i++)
+        {
+            ans[i]=count[i]%1000;
+        }
+        return ans; 
+    }
+}
