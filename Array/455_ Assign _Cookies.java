5ca8e382267c52202885d43a603ca1e78e615830
Shivam Jaiswal <jaiswalshivam373@gmail.com>
2022-05-24 22:50:16 +0530

    455

---
+++ Array/455_ Assign _Cookies.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+         Arrays.sort(g);
+      Arrays.sort(s);
+        int pointG = 0,pointS = 0;
+        while (pointG<g.length && pointS<s.length) 
+        {
+            if (g[pointG]<=s[pointS]) 
+            {
+                pointG++;
+                pointS++;
+            } 
+            else 
+            {
+                pointS++;
+            }
+        }
+        return pointG;   
+    }
+}
