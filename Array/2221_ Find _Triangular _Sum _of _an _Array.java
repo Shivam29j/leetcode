58a41db5f0f55cde9319eb3e509bf0b25b545eba
Shivam Jaiswal <jaiswalshivam373@gmail.com>
2022-05-26 21:53:55 +0530

    2221

---
+++ Array/2221_ Find _Triangular _Sum _of _an _Array.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int triangularSum(int[] nums) {
+         int n=nums.length;
+        for(int i=0;i<n-1;i++)
+        {
+            for(int j=0;j<n-1-i;j++)
+            {
+                nums[j]=(nums[j]+nums[j+1])%10;
+            }
+        }
+        return nums[0];
+    }
+}
