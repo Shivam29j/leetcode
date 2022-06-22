36a45e459618fe7b98084bed9f7b18e285b00b67
Shivam Jaiswal <jaiswalshivam373@gmail.com>
2022-06-22 10:19:34 +0530

    492

---
+++ Array/492_Construct_ the _Rectangle.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] constructRectangle(int area) {
+         int[] result = new int[2];
+        if(area == 0){
+            return result;
+        }
+        int a = (int)Math.sqrt(area);
+        while(area%a != 0){
+            a--;
+        }
+        int b = area/a;
+        result[0] = b;
+        result[1] = a;
+        return result;
+    }
+}
