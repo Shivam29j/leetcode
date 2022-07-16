ca31f62de183a60355c3160acbaa4df5b428b596
Shivam Jaiswal <jaiswalshivam373@gmail.com>
2022-07-16 23:17:09 +0530

    38

---
+++ Array/38_ Count _and _Say.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String countAndSay(int n) {
+        if(n==1) 
+          return "1";
+      if(n==2) 
+          return "11";
+      String str="11";
+      for (int i=3;i<=n;++i) {
+          int cnt=1;
+          str+="a";   
+          String res="";
+          char[] array = str.toCharArray();
+          for(int j=1;j<array.length;++j) {
+              if(array[j]!=array[j-1]) {
+                  res += cnt;
+                  res += array[j-1];
+                  cnt=1;
+              }
+              else
+                  cnt++;
+          }
+          str=res;            
+      }
+      return str; 
+    }
+}
