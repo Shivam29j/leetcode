3cab949d5966b47e91047102ba3d3860622ca94c
Shivam Jaiswal <jaiswalshivam373@gmail.com>
2022-10-05 23:05:50 +0530

    factorial

--- turncate_sentence.java
+++ turncate_sentence.java
@@ -1,11 +1,11 @@
 class Solution {
     public String truncateSentence(String s, int k) {
-         int space = 0;
+        int space = 0;
         for(int i = 0 ; i< s.length(); i++){
             if(s.charAt(i) == ' '){
-             space +=1;  
-             if(space == k){ 
-             return s.substring(0, i);  
+                space +=1;
+                if(space == k){
+                    return s.substring(0, i);
                 }
             }
         }
